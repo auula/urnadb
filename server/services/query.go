c27628417a0744dc9641e5e5d37951d1c67baeef
Leon Ding <ding_ms@outlook.com>
2025-10-29 02:16:13 +0800

    fix: query service indented json output.

--- server/services/query.go
+++ server/services/query.go
@@ -1,7 +1,23 @@
 package services
 
-import "github.com/auula/urnadb/vfs"
+import (
+	"github.com/auula/urnadb/vfs"
+)
 
 type QueryService interface {
 	GetSegment(name string) (version uint64, seg *vfs.Segment, err error)
+}
+
+type QueryServiceImpl struct {
+	storage *vfs.LogStructuredFS
+}
+
+func NewQueryServiceImpl(storage *vfs.LogStructuredFS) QueryService {
+	return &QueryServiceImpl{
+		storage: storage,
+	}
+}
+
+func (q *QueryServiceImpl) GetSegment(name string) (version uint64, seg *vfs.Segment, err error) {
+	return q.storage.FetchSegment(name)
 }
